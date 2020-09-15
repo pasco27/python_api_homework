7e3421ab84c4d35b0808e95878132fcd10c3b95b
Rob Pascarella <robpascarella@Robs-iMac.local>
2020-09-14 23:20:29 -0500

    starting to narrow city data for vacationpy

--- .ipynb_checkpoints/api_keys-checkpoint.py
+++ .ipynb_checkpoints/api_keys-checkpoint.py
@@ -3,3 +3,5 @@ weather_api_key = "a18023732a8ff6de5a1f2bad3b1e3ce5"
 
 # Google API Key
 g_key = "YOUR KEY HERE!"
+
+### I think I want to make the g_key a separate file that I don't upload to github
