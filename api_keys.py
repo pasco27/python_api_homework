5183f5ec5ab6b83fcf593f35b2b8759d1acdb47d
Rob Pascarella <58762374+pasco27@users.noreply.github.com>
2020-09-27 14:03:25 -0500

    Delete api_keys.py

--- api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "a18023732a8ff6de5a1f2bad3b1e3ce5"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
-
-### I think I want to make the g_key a separate file that I don't upload to github
